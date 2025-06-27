a95dccf05a91b55789e50b5cd7b3e292f65ccd8f
EthanLeRoux <127761539+EthanLeRoux@users.noreply.github.com>
2025-06-27 15:59:18 +0200

    Update logo image path and modify navigation links in HomeNavigation component

--- src/components/HomeNavigation.jsx
+++ src/components/HomeNavigation.jsx
@@ -6,13 +6,13 @@ export default function RootNavigation(){
         <>
             <div className="NavigationLinks">
                 <div className="nav-left">
-                    <img src={"h2.png"} alt={"Hanashi Logo"} />
+                    <img src={"/h2.png"} alt={"Hanashi Logo"} />
                     <Link to={'/'} className="Logo">Hanashi</Link>
                 </div>
 
                 <div className="nav-right">
                     {/*<Link to="/features" className="Links">Features</Link>*/}
-                    <Link to="/docs" className="Links">Docs</Link>
+                    {/*<Link to="/docs" className="Links">Docs</Link>*/}
                     <a href="https://hanashi.website/stories" target="_blank" className="navigation-link-goto">Go to Dashboard</a>
                 </div>
 
